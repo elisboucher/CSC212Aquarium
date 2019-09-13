433c89d817951f074bc1d2c79deaacafa4f029a7
Elise <Elise@131.229.196.77>
2019-09-13 14:24:41 -0400

    Working on the lab. Creation of Fish class.

--- src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
+++ src/main/java/edu/smith/cs/csc212/aquarium/Aquarium.java
@@ -41,29 +41,34 @@ public class Aquarium extends GFX {
 		super(WIDTH, HEIGHT);
 	}
 
+	Fish fish1 = new Fish(Color.pink,250,250); 
 	int fish1X = getWidth() + 100;
 	int fish2X = getWidth() + 300;
+	int fish3x = getWidth() + 200;
 
 	@Override
 	public void draw(Graphics2D g) {
 		// Draw the "ocean" background.
 		g.setColor(Color.blue);
 		g.fillRect(0, 0, getWidth(), getHeight());
-
+		fish1.draw(g); 
+		nemo.draw(g);
 		// Draw the fish!
 		DrawFish.facingLeft(g, Color.yellow, fish1X, 200);
 		// Draw the confused fish!
 		DrawFish.facingRight(g, Color.green, fish2X, 300);
 
 		// What if we wanted this little fish to swim, too?
-		DrawFish.smallFacingLeft(g, Color.red, 200, 100);
-
+		DrawFish.smallFacingLeft(g, Color.red, fish3x, 100);
+		
+		DrawFish.facingLeft(g,fish1.color, fish1.x,fish1.y);
 		// Draw our snail!
 		algorithm.draw(g);
 
 		// Move the fish!
 		fish1X -= 1;
 		fish2X -= 2;
+		fish3x -= 3; 
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -0,0 +1,19 @@
+package edu.smith.cs.csc212.aquarium;
+import java.awt.Color;
+
+public class Fish {
+	 int x,y;
+	 Color color;
+	
+	 //Construct fish at position with color
+	public Fish(Color c ,int startX, int startY) {
+		this.x=startX;
+		this.y=startY;
+		this.color= c;
+		
+	}	
+		
+		
+	
+
+}
